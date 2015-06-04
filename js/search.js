c6407858edf60f8d686d73f55bcd2c4b1d423646
bigcamo <nicolasguth67@gmail.com>
2015-06-04 09:09:40 +0200

    Ajaxification du search

---
+++ js/search.js
@@ -0,0 +1,19 @@
+$(document).ready(function() // une fois le document chargé
+{
+	$('.search').keyup(function() // on selection le champ 'search' qui, dès qu'on relache une touche, va lire la fonction. 
+	{
+		input_results = $(this).val(); // on assigne a input_results le résultat de l'input
+		//alert(input_results);
+		if( input_results.length > 2) // on utilise ajax partir du moment ou on a entré minimum 1 caratère
+		{
+			$.post('index.php?ajax=search', // On fait le lien vers l'index.php (qui utilise la méthode POST)
+			{
+				"search" : input_results // on envoie le résultat de (input_results) de notre input (qui a la classe search)
+				//data : $(this).val(),
+			}, function(data)
+			{
+				$('.container.body.section').html(data);
+			});
+		}
+	});
+});
