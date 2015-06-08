76dce47f243fc44ce57e9171015cef327038c0bc
Plegis <nicomar1@hotmail.fr>
2015-06-08 14:24:13 +0200

    maj panier avec ajout du fichier panier.phtml

--- panier/index.php
+++ panier/index.php
@@ -1,20 +1,42 @@
 <?php
 
+
 if(isset($_GET['order_id'])){
+	// Chemin de la requète & jointure.
+	$request = "SELECT *
+				FROM `orders` os
+				JOIN `order_item` oi
+				ON os.`id`= oi.`id_order` WHERE id_order=".$_GET['order_id'];
+	//order_id = données transmises lors de l'ajout au panier
 
-	$request = $db->query("SELECT * FROM orders WHERE id=".$_GET['orders_id'] AND $_GET['orders_id'] = 2);
-	
+	// Récupération des données dans un tableau.
+	$order = $db->query($request)->fetchAll(PDO::FETCH_ASSOC);
 
-	$order = $db->query($request)->fetch(PDO::FETCH_ASSOC);
-	if(empty($order) || $order['status'] == 0 || $order['status'] == 1){
-		echo "Il n'y a pas d'article dans le panier";
-	}else{
-		$panier[add($status[1])];
-		echo 'Le produit a bien été ajouté à votre panier';
 
+	// Si les données que l'on récupère ont un status qui est égal à deux,...
+	if($order[!NULL]['status'] == 2){
+		// ... Alors on récupère tous les tableaux :
+		$i = 0;
+		// Tant que $i est inférieur à la longueur du tableau...
+		while($i < sizeof($order))
+		{
+			// ... On affiche :
+			$id_product = $order[$i]["id_product"];
+			$weight = $order[$i]["weight"];
+			$price_per_kilo = $order[$i]["price_per_kilo"];
+			$price_total = $order[$i]["price_total"];
+			$i++;
+			require("panier/panier.phtml");
+		}
+	require ("panier/index.phtml");
+	}
+	else
+	{
+		echo "Il n'y a pas de commande ayant cet ID";
 	}
 }else{
 	echo "Vous n'avez pas sélectionné de produit à ajouter au panier";
 }
-require ("panier/index.phtml");
+
+
 ?>
