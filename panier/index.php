25b74177a285ffae8e0aecceba1f9f0e0d8855d6
Plegis <nicomar1@hotmail.fr>
2015-06-08 09:21:02 +0200

    vérif css

--- panier/index.php
+++ panier/index.php
@@ -1,29 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
 <?php
 
 if(isset($_GET['order_id'])){
 
-	$request = $db->query("SELECT * FROM order WHERE id=".$_GET['order_id']);
-	// ajouter le status de la commande
+	$request = $db->query("SELECT * FROM orders WHERE id=".$_GET['orders_id'] AND $_GET['orders_id'] = 2);
+	
 
 	$order = $db->query($request)->fetch(PDO::FETCH_ASSOC);
-	if(empty($order) || $_GET['status'] == 0 || $_GET['status'] == 2){
+	if(empty($order) || $order['status'] == 0 || $order['status'] == 1){
 		echo "Il n'y a pas d'article dans le panier";
 	}else{
-		$panier['add($status[1])'];
+		$panier[add($status[1])];
 		echo 'Le produit a bien été ajouté à votre panier';
+
 	}
 }else{
 	echo "Vous n'avez pas sélectionné de produit à ajouter au panier";
 }
+require ("panier/index.phtml");
 ?>
-
-	
-</body>
-</html>
