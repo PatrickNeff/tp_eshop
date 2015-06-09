546fbbcc042c6f51157ec455bc875aaff2c19aaf
Plegis <nicomar1@hotmail.fr>
2015-06-09 13:31:08 +0200

    ajout et maj jquery

---
+++ js/panier.js
@@ -0,0 +1,17 @@
+$(document).ready(function() // une fois le document chargé
+{
+	$("#valider").click(function(){
+		$(location).attr('href',"index.php?page=validate")
+	});
+	$("#retour").click(function(){
+		$(location).attr('href',"index.php?page=catalogue")
+	});
+
+	$("#annuler").click(function(){
+	    $.ajax({
+	       url : 'panier/annuler.php',
+	       type : 'POST',
+	       dataType : 'php'
+	    });
+	});
+});
