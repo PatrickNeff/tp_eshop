66a11d759e651417d18a64d00ad5dda1d71817f5
bigcamo <nicolasguth67@gmail.com>
2015-06-09 09:53:47 +0200

    envoie variable

--- panier/annuler.php
+++ panier/annuler.php
@@ -1,14 +1,13 @@
 <?php
 var_dump($_POST);
 
-
-// ON ne récupère pas encore la variable.
-// var_dump($order);
+var_dump($id_product);
 
 
 // DELETE * FROM product WHERE id_product= ??
 
 
 // Redirection vers index.php?page=panier&order_id=6;
-//header('Location: index.php?page=panier&order_id=6');
+// header('Location: index.php?page=panier&order_id=6');
+
 ?>
