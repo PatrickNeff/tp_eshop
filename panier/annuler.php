2bc0c3791665e61f6ea75f4add81d58422c8d952
Plegis <nicomar1@hotmail.fr>
2015-06-09 09:34:01 +0200

    ajout de nouveaux fichiers

---
+++ panier/annuler.php
@@ -0,0 +1,14 @@
+<?php
+var_dump($_POST);
+
+
+// ON ne récupère pas encore la variable.
+// var_dump($order);
+
+
+// DELETE * FROM product WHERE id_product= ??
+
+
+// Redirection vers index.php?page=panier&order_id=6;
+//header('Location: index.php?page=panier&order_id=6');
+?>
