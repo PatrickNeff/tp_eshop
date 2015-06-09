cd4d35f20801492a38fb8f5cef7b2e399dc05af9
bigcamo <nicolasguth67@gmail.com>
2015-06-09 17:02:48 +0200

    panier ok

--- panier/index.php
+++ panier/index.php
@@ -1,13 +1,15 @@
 <?php
 
-if(isset($_GET['order_id'])){
+if(isset($_GET['order_id']))
+{
 	// Chemin de la requète & jointure.
-	$request = "SELECT *
+
+	$request = "SELECT *, oi.id AS my_id
 				FROM `order_item` oi
 				JOIN `orders` os
 				ON os.`id`= oi.`id_order` 
 				JOIN `product` po
-				ON po.`id_product`=oi.`id_product` WHERE id_order=".$_GET['order_id'];
+				ON po.`id_product`=oi.`id_product` WHERE os.status = '2' AND id_order=".$_GET['order_id'];
 	//order_id = données transmises lors de l'ajout au panier
 	// var_dump($request);
 	// Récupération des données dans un tableau.
@@ -15,20 +17,17 @@ if(isset($_GET['order_id'])){
 
 	// var_dump($order);
 
-	$request = $db->query("SELECT * FROM orders WHERE id=".$_GET['order_id']);
-	// ajouter le status de la commande
-
-
 	// Si les données que l'on récupère ont un status qui est égal à deux,...
-	if($order[!NULL]['status'] == 2){
-		// ... Alors on récupère tous les tableaux :
+
 		$i = 0;
 		$super_total =0;
 		// Tant que $i est inférieur à la longueur du tableau...
 		while($i < sizeof($order))
 		{
 			// ... On affiche :
-			$id_product = $order[$i]["name"];
+			$my_id = $order[$i]["my_id"];
+			$id_product = $order[$i]["id_product"];
+			$name = $order[$i]["name"];
 			$weight = number_format($order[$i]["weight"],3,',',' ');
 			$price_per_kilo = number_format($order[$i]["price_per_kilo"],2,',',' ');
 			$price_total = number_format($order[$i]["price_total"],2,',',' ');
@@ -38,16 +37,11 @@ if(isset($_GET['order_id'])){
 		}
 
 	require ("panier/index.phtml");
-	}
-	else
-	{
-		echo "Il n'y a pas de commande ayant cet ID";
-	}
-}else{
+
+}
+else{
 	echo "Vous n'avez pas sélectionné de produit à ajouter au panier";
 }
-
-
 
 // On vérifie  que $_GET['action'] (défini dans le formulaire de panier.phtml) existe.
 if (isset($_GET['action']))
@@ -59,5 +53,6 @@ if (isset($_GET['action']))
 		require('panier/annuler.php');
 	}
 }
+
 
 ?>
